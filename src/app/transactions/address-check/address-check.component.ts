d8b1afa59a5aec7ad6afd26e5ad69a8e62eec596
Suresh Subramani <suresh.subramani@vodafone.com>
2022-01-03 16:20:31 +0000

    Split window option

--- src/app/transactions/address-check/address-check.component.ts
+++ src/app/transactions/address-check/address-check.component.ts
@@ -1,10 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-export interface Tile {
+export interface Tile1 {
   color: string;
   cols: number;
   rows: number;
-  text: string;
+  text: string;  
 }
+export interface Tile {  
+  text: string;
+  class:string;
+}
+
+export interface IAddressCheck {
+  errorCode: string;
+  checkedAddress: string;
+  pafAddress: string;
+  inputAddress1: string;
+  inputAddress2: string;
+  inputAddress3: string;
+  inputAddress4: string;
+  inputPostCode: string;
+  inputValidation: string;
+  pafAddress1: string;
+  pafAddress2: string;
+  pafAddress3: string;
+  pafAddress4: string;
+  pafPostCode: string;
+  pafValidation: string;
+  btCheckedAddressPremises: string;
+  btCheckedAddressThoroughfare: string;
+  btCheckedAddressLocality: string;
+  btCheckedAddressAddressOverflow: string;
+  btCheckedAddressOverError: string;
+}
+
 
 @Component({
   selector: 'app-address-check',
@@ -12,16 +40,77 @@ export interface Tile {
   styleUrls: ['./address-check.component.css']
 })
 export class AddressCheckComponent implements OnInit {
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    
+  summaryTiles: Tile[] = [    
+    {text: 'Error Code' ,class:"vf-sub-title"},
+    {text: 'Checked Address',class:"vf-sub-title"},
+    {text: 'PAF Address',class:"vf-sub-title"},
+    {text: 'Error Code value',class:"vf-grid-value"},
+    {text: 'Checked Address value',class:"vf-grid-value"},
+    {text: 'PAF Address value',class:"vf-grid-value"},
+  ];
+  pafTiles: Tile[] = [
+    {text: 'Input Format'        ,class:"vf-sub-title"},
+    {text: 'Input Address'       ,class:"vf-sub-title"},
+    {text: 'PAF Address'         ,class:"vf-sub-title"},
+    {text: 'Address1'            ,class:"vf-sub-title"},
+    {text: 'Address1 value'      ,class:"vf-grid-value"},
+    {text: 'PAF Address1 value'  ,class:"vf-grid-value"}, 
+    {text: 'Address2'            ,class:"vf-sub-title"},
+    {text: 'Address2 value'      ,class:"vf-grid-value"},
+    {text: 'PAF Address2 value'  ,class:"vf-grid-value"}, 
+    {text: 'Address3'            ,class:"vf-sub-title"},
+    {text: 'Address3 value'      ,class:"vf-grid-value"},
+    {text: 'PAF Address3 value'  ,class:"vf-grid-value"}, 
+    {text: 'Address4'            ,class:"vf-sub-title"}, 
+    {text: 'Address4 value'      ,class:"vf-grid-value"}, 
+    {text: 'PAF Address4 value'  ,class:"vf-grid-value"},
+    {text: 'Postcode'            ,class:"vf-sub-title"},
+    {text: 'Postcode value'      ,class:"vf-grid-value"}, 
+    {text: 'PAF Postcode value'  ,class:"vf-grid-value"}, 
+    {text: 'Validation'          ,class:"vf-sub-title"}, 
+    {text: 'Validation value'    ,class:"vf-grid-value"},
+    {text: 'PAF Validation value',class:"vf-grid-value"},
+  ];
+  btTiles: Tile[] = [
+    {text: 'BT Format'                      ,class:"vf-sub-title"},
+    {text: 'BT Checked Address'             ,class:"vf-sub-title"},
+    {text: 'BT Checked PAF Address'         ,class:"vf-sub-title"},
+    {text: 'Premises'            ,class:"vf-sub-title"},
+    {text: 'BT Premises value'      ,class:"vf-grid-value"},
+    {text: 'PAF Premises value'  ,class:"vf-grid-value"}, 
+    {text: 'Throughfare'            ,class:"vf-sub-title"},
+    {text: 'BT Throughfare value'      ,class:"vf-grid-value"},
+    {text: 'PAF Throughfare value'  ,class:"vf-grid-value"}, 
+    {text: 'Locality'            ,class:"vf-sub-title"},
+    {text: 'BT Locality value'      ,class:"vf-grid-value"},
+    {text: 'PAF Locality value'  ,class:"vf-grid-value"}, 
+    {text: 'Address Overflow'            ,class:"vf-sub-title"}, 
+    {text: 'BT Address Overflow value'      ,class:"vf-grid-value"}, 
+    {text: 'PAF Address Overflow value'  ,class:"vf-grid-value"},
+    {text: 'overflow Error'            ,class:"vf-sub-title"},
+    {text: 'BT overflow Error value'      ,class:"vf-grid-value"}, 
+    {text: 'PAF overflow Error value'  ,class:"vf-grid-value"},
   ];
 
+  dataSource!: IAddressCheck;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ReturnAddress()
+  {
+    window.alert("Return Address");
+  }
+  Help()
+  {
+    window.alert("Help");
+  }
+  Print()
+  {
+    window.alert("Print");
   }
 
 }
--- src/app/transactions/transactions-views/transactions-views.component.ts
+++ src/app/transactions/transactions-views/transactions-views.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { CupId } from 'src/app/_data/listValues/CupId';
 import { TableItem } from 'src/app/_models/table-item';
 import { take } from 'rxjs/operators';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-transactions-views',
@@ -15,19 +16,28 @@ export class TransactionsViewsComponent implements OnInit {
   view1Toggle: string ="";
   view2Toggle: string ="";
   view3Toggle: string="";
-  StartTelNo:string = "";
-  EndTelNo:string="";
+  StartTelNo:string = "01234567890";
+  EndTelNo:string="01234567890";
   Provide:string="000000";
   Master:string="000000";
   Live:string="000000";
   selectedCupId:number=0;
-  selectedFrancise:number=0;  
+  selectedFranchise:number=0;  
   isEndTelNo:Boolean =false;
-  cupIds:any =new CupId().cupIds;   
+  cupIds:any =new CupId().cupIds; 
+  searchTelState:boolean=true;
+  saveState:boolean=true;
+  views:any={view1:false,view2:false,view3:false}  
+  enableFrancise:boolean=false;
 
-    model:any ={tel:"",rangeEnd:""};
+    model:any ={tel:"",rangeEnd:"",CupId:"",Franchise:""};
     @Output() AddressCheckSelected = new EventEmitter<any[]>();
     @Output() AuditTrailSelected = new EventEmitter<any[]>();
+    @Output() ResetTabs = new EventEmitter<any[]>();
+
+    panelOpenState = false;
+    btncolor: string ="secondary"
+    savebtnColor:string ="secondary"
     
 
   constructor(private _ngZone: NgZone)  {}
@@ -35,10 +45,13 @@ export class TransactionsViewsComponent implements OnInit {
     throw new Error('Method not implemented.');    
   }
 
-  ngOnInit() {    
-    this.view1Toggle ="display: block;visibility:visible;";
-    this.view2Toggle ="display: none;visibility:hidden;";
-    this.view3Toggle ="display: none;visibility:hidden;";  
+  ngOnInit() {  
+    this.views.view1=true;  
+    // this.view1Toggle ="display: block;visibility:visible;";
+    // this.view2Toggle ="display: none;visibility:hidden;";
+    // this.view3Toggle ="display: none;visibility:hidden;";  
+    // this.model.tel ="01234567890";
+    // this.model.rangeEnd ="01234567890";
     }
 
   @ViewChild('autosize')
@@ -56,6 +69,30 @@ export class TransactionsViewsComponent implements OnInit {
 {
   
 }
+
+onChangeEvent(event:any)
+{
+  if(event.target.value !="")
+  {
+    // console.log(this.searchTelState,this.btncolor)
+    // console.log(event.target.value);
+    this.searchTelState =false;
+    this.btncolor ="vf-primary-btn";
+    // console.log(event.target.value); 
+    // console.log(this.searchTelState,this.btncolor)   
+
+  }
+
+}
+
+OnstateItemChange(event:any)
+{
+  if(event.target.value !="")
+  {    
+    this.saveState =false;
+    this.savebtnColor ="vf-primary-btn";   
+  }
+}
 check_list(this:TableItem,val:number)
 {
 
@@ -66,7 +103,7 @@ check_text(this:TableItem,val:number,val2:string,val3:string)
 }
 viewAddressCheck()
 {
-  this.AddressCheckSelected.emit(["true"])
+  this.AddressCheckSelected.emit(["true",this.model.telno])
 }
 sysEditText(val:string)
 {
@@ -76,21 +113,35 @@ saveTran(val:number)
 {
   
 }
-  SearchTel(telno:string,rangeEnd:string)
-  { 
-    this.StartTelNo = telno.trim();
-    this.EndTelNo = rangeEnd.trim();
-    this.isEndTelNo =this.EndTelNo !="";
-    
-    this.view2Toggle ="display: block;visibility:visible;";
-    this.view1Toggle ="display: none;visibility:hidden;";
-    this.view3Toggle ="display: none;visibility:hidden;";
+SearchTel(){ 
+    if(this.model.tel !="" ||this.model.rangeEnd !="")
+      {
+        //console.log(this.model.tel,this.model.rangeEnd )   
+          //this.view2Toggle ="display: block;visibility:visible;";
+          //this.view1Toggle ="display: none;visibility:hidden;";
+          // this.view3Toggle ="display: none;visibility:hidden;";
+          this.views.view1=false;
+          this.views.view2 =true;
+          this.views.view3 =false;
+          this.panelOpenState =true;
+      }
   }
   resetTel(sf:any) {
     
-    this.view1Toggle ="display: block;visibility:visible;";
-    this.view2Toggle ="display: none;visibility:hidden;";
-    this.view3Toggle ="display: none;visibility:hidden;";
+    // this.view1Toggle ="display: block;visibility:visible;";
+    // this.view2Toggle ="display: none;visibility:hidden;";
+    // this.view3Toggle ="display: none;visibility:hidden;";
+    this.model={tel:"",rangeEnd:"",CupId:"",Franchise:""};
+    this.views.view3=false;
+    this.views.view2=false;
+    this.views.view1=true;
+    this.searchTelState =true;
+    this.btncolor ="secondary";
+    this.saveState =true;
+    this.savebtnColor ="secondary";
+    this.enableFrancise =false;
+    this.ResetTabs.emit(["true"]);
+    
   }   
   resetTel1(sf:any) {
     
@@ -100,22 +151,38 @@ saveTran(val:number)
   }  
   AuditTrail()
   {
-    this.AuditTrailSelected.emit(["true"])
+    this.AuditTrailSelected.emit(["true",this.model.telno,this.model.rangeEnd])
   }
-  check_cupid(this: any)
+  onCupIdChange (event:any)
   {
-
+    if(event.value !="")
+    {
+      this.enableFrancise=true;
+      //load francise with this value
+    }
   }
-  cupid_selected(this: any)
-  {
-
-  }
-  check_franchise(tf: any)
-  {    
+  check_franchise()
+  {  
+    this.views.view3=true; 
+    // this.panelOpenState =false; 
     // if(tf.franchise.selected )
     //   this.view3Toggle ="display: block;visibility:visible;";
     // else
     // this.view3Toggle ="display: none;visibility:hidden;";
+    // console.log( this.model.Franchise );
+    // if (this.model.Franchise != undefined)
+    //   {
+    //     console.log(this.model.CupId,this.model.Franchise )
+    //     this.views.view2=true;
+    //     this.views.view1=false;
+    //     this.views.view3=false;
+       
+    //   }
+    //   else{
+    //     this.views.view2=true;
+    //     this.views.view1=false;
+    //     this.views.view3=false;
+    //   }
   }
 
   loadview(viewNumber:number)
