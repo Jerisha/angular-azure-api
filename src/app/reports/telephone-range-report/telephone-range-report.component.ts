a5f9ebbf82856c808ffa3ebe2cdf8096bf284c49
Dev Joshi <dev.joshi@vodafone.com>
2022-01-27 06:32:34 +0000

    Before merege to dev 27-01-2022

--- src/app/reports/telephone-range-report/telephone-range-report.component.ts
+++ src/app/reports/telephone-range-report/telephone-range-report.component.ts
@@ -1,32 +1,37 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TableSelectionComponent } from 'src/app/uicomponents';
 import { ColumnDetails, TableItem } from 'src/app/_models/table-item';
+import { TelephoneRangeReport } from 'src/app/_models/telephone-range-report-model';
 
 const ELEMENT_DATA = [
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
   },
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
   },
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
   },
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
   },
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
   },
   {
-    StartTel:'02079445797',EndTel:'02079446999',Live:'1,203',Trans:'',Null:'',Line:'D',Name:'DEPARTMENT OF TRANSPORT',
-    Address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',Source:'C-SAS/COMS'
-  }
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
+  },
+  {
+    startTel:'02079445797',endTel:'02079446999',live:'1,203',trans:'',null:'',line:'D',name:'DEPARTMENT OF TRANSPORT',
+    address:'HOUSE, 33 33 HORSEFERRY RD/ 7, LONDON,MIDDLESEX, SW1P 4DR',source:'C-SAS/COMS',orderRef:''
+  },
 ];
 
 @Component({
@@ -38,22 +43,43 @@ export class TelephoneRangeReportComponent implements OnInit {
 
   @ViewChild('table1') table1?:TableSelectionComponent;
   myTable!: TableItem;
-  constructor() { }
-  columns: ColumnDetails[] =[
-    { header: 'StartTel', headerValue: 'StartTel', showDefault: true, imageColumn: false },
-    { header: 'EndTel', headerValue: 'EndTel', showDefault: true, imageColumn: false },
-    { header: 'Live', headerValue: 'Live', showDefault: true, imageColumn: false },
-    { header: 'Trans', headerValue: 'Trans', showDefault: true, imageColumn: false },
-    { header: 'Null', headerValue: 'Null', showDefault: true, imageColumn: false },
-    { header: 'Line', headerValue: 'Line', showDefault: true, imageColumn: false },
-    { header: 'Name', headerValue: 'Name', showDefault: true, imageColumn: false },
-    { header: 'Address', headerValue: 'Address', showDefault: true, imageColumn: false },
-    { header: 'Source', headerValue: 'Source', showDefault: true, imageColumn: false },
+  dataSaved = false;
+  selectListItems: string[] = [];
+
+  selectedTab!: number;
+  public tabs = [{
+    tabType: 0,
+    name: 'Summary'
+  },
+    //  {
+    //   tabType: 1,
+    //   name: 'Audit Trail Report'
+    // },{
+    //   tabType: 2,
+    //   name: 'Transaction Details'
+    // }
   ];
+
+  
+  columns: ColumnDetails[] =[
+    { header: 'Start Tel. No.', headerValue: 'startTel', showDefault: true, imageColumn: false },
+    { header: 'End Tel. No.', headerValue: 'endTel', showDefault: true, imageColumn: false },
+    { header: 'Source System', headerValue: 'source', showDefault: true, imageColumn: false },
+    { header: 'Line Type', headerValue: 'line', showDefault: true, imageColumn: false },
+    { header: 'Live Records', headerValue: 'live', showDefault: true, imageColumn: false },
+    { header: 'Trans', headerValue: 'trans', showDefault: true, imageColumn: false },
+    { header: 'Not Available', headerValue: 'null', showDefault: true, imageColumn: false },
+    { header: 'Customer Name', headerValue: 'name', showDefault: true, imageColumn: false },
+    { header: 'Customer Address', headerValue: 'address', showDefault: true, imageColumn: false },
+    { header: 'Order Ref', headerValue: 'orderRef', showDefault: true, imageColumn: false },
+  ];
+  data1:TelephoneRangeReport[] = ELEMENT_DATA;
+
+  constructor() { }
 
   ngOnInit(): void {
     this.myTable = {
-      data: ELEMENT_DATA,
+      data: this.data1,
       Columns: this.columns,
       filter: true,
       selectCheckbox: true,
@@ -67,6 +93,58 @@ export class TelephoneRangeReportComponent implements OnInit {
 
   }
   resetForm():void{
+  }
+  rowDetect(item: any) {
+    //debugger;
+    if (item.length == 0) {
+      this.selectListItems = [];
+    } else {
+      item.forEach((el: string) => {
+        if (!this.selectListItems.includes(el)) {
+          this.selectListItems.push(el)
+        }
+        else {
+          if (this.selectListItems.includes(el)) {
+            let index = this.selectListItems.indexOf(el);
+            this.selectListItems.splice(index, 1)
+          }
+        }
+      });
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+
+  newTab(tab: any) {
+    switch (tab.tabType) {
+      case 1: {
+        //tab.row contains row data- fetch data from api and bind to respetive component
+        if (!this.tabs.find(x => x.tabType == 1)) {
+          this.tabs.push({
+            tabType: 1,
+            name: 'Audit Trail Report (1977722725)'
+          });
+          this.selectedTab = 1;
+        }
+        break;
+      }
+      case 2: {
+        if (!this.tabs.find(x => x.tabType == 2)) {
+          this.tabs.push({
+            tabType: 2,
+            name: 'Transaction Details'
+          })
+          this.selectedTab = 2;
+        }
+        break;
+      }
+      default: {
+        //statements; 
+        break;
+      }
+    }
   }
 
 }
