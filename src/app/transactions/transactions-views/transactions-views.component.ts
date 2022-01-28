3b89e065699a08bfcb33ad802ba071230afaf228
Suresh Subramani <suresh.subramani@vodafone.com>
2022-01-28 17:20:36 +0000

    Tab-style_other_changes

--- src/app/transactions/transactions-views/transactions-views.component.ts
+++ src/app/transactions/transactions-views/transactions-views.component.ts
@@ -11,6 +11,7 @@ import { ThrowStmt } from '@angular/compiler';
   templateUrl: './transactions-views.component.html',
   styleUrls: ['./transactions-views.component.css']
 })
+
 export class TransactionsViewsComponent implements OnInit {
   
   view1Toggle: string ="";
@@ -41,6 +42,8 @@ export class TransactionsViewsComponent implements OnInit {
     @Output() AddressCheckSelected = new EventEmitter<any[]>();
     @Output() AuditTrailSelected = new EventEmitter<any[]>();
     @Output() ResetTabs = new EventEmitter<any[]>();
+    
+    CliRangeSet: [number, number][] = [];
 
     panelOpenState = false;
     btncolor: string ="secondary"
@@ -85,6 +88,9 @@ export class TransactionsViewsComponent implements OnInit {
   onSubmit()
 {
   
+}
+removeRangeCli(){
+
 }
 
 onChangeEvent(event:any)
@@ -132,15 +138,23 @@ sysEditText(val:string)
 }
 saveTran(val:number)
 {
+          
   
 }
+ReviewCli()
+{
+  this.views.view1=true;
+  this.views.view2 =false;
+  this.views.view3 =false;
+}
 SearchTel(){ 
-    if(this.model.tel !="" ||this.model.rangeEnd !="")
+    if(this.model.tel !="" ||this.model.rangeEnd !="" ||this.CliRangeSet.length>0)
       {
         //console.log(this.model.tel,this.model.rangeEnd )   
           //this.view2Toggle ="display: block;visibility:visible;";
           //this.view1Toggle ="display: none;visibility:hidden;";
           // this.view3Toggle ="display: none;visibility:hidden;";
+          this.CliRangeSet.push([this.model.tel,this.model.rangeEnd]);
           this.views.view1=false;
           this.views.view2 =true;
           this.views.view3 =false;
@@ -162,6 +176,7 @@ SearchTel(){
     this.savebtnColor ="secondary";
     this.enableFrancise =false;
     this.ResetTabs.emit(["true"]);
+    this.CliRangeSet=[];
     
   }   
   resetTel1(sf:any) {
@@ -184,7 +199,11 @@ SearchTel(){
   }
   addRangeTel()
   {
-    alert('add telephone range!');
+    // alert('add telephone range!');
+    this.CliRangeSet.push([this.model.tel,this.model.rangeEnd])
+    this.model ={tel:"",rangeEnd:"",CupId:"",Franchise:""};
+    
+
   }
   check_franchise()
   {  
