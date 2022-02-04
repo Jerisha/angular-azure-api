512a5c2d933fb44ee46a0e2a02449096036bb0b0
Ashok Kumar R <Ashok.Kumarr@vodafone.com>
2022-02-04 07:05:44 +0000

    wrapper layer and styles updated

--- src/app/_services/http/http-wrapper.service.ts
+++ src/app/_services/http/http-wrapper.service.ts
@@ -5,12 +5,14 @@ import { environment } from 'src/environments/environment';
 import { ResponseType } from 'src/app/_enums/response-type.enum';
 import { HttpVerbs } from 'src/app/_enums/http-verbs.enum';
 import { WebMethods } from 'src/app/_enums/web-methods.enum';
+import { WMMessageType } from 'src/app/_enums/wmmessage-type.enum';
+import { Router } from '@angular/router';
 
 
 @Injectable({ providedIn: 'root' })
 export class HttpWrapperService {
 
-    constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient, private _route: Router) {
     }
 
     processRequst<Type>(httpVerb: HttpVerbs, endPoint: WebMethods, body: {}, headers?: HttpHeaders, params?: HttpParams, responseType = ResponseType.JSON):
@@ -51,17 +53,18 @@ export class HttpWrapperService {
 
 
     private resolveRespone(val: any, requestType: WebMethods): any {
-        
         debugger;
         let categories = [];
         let jsonResult = '';
         switch (requestType) {
             case WebMethods.CONFIG:
                 categories = val.ConfigObjectResponseType.ListofConfigObjectCategory.ConfigObjectCategory;
+                this.validateResponseStatus(this.checkResponseStatus(categories));
                 jsonResult = this.processConfigObject(categories);
                 break;
             case WebMethods.QUERY:
                 categories = val.QueryObjectResponseType.ListofQueryObjectCategory.QueryObjectCategory;
+                this.validateResponseStatus(this.checkResponseStatus(categories));
                 jsonResult = this.processQueryObject(categories);
                 break;
             case WebMethods.GET:
@@ -175,5 +178,43 @@ export class HttpWrapperService {
             }
         });
         return jsonCreation;
+    }
+
+    private checkResponseStatus(categories: any) {
+        var jsonCreation = `[`
+        if (categories != undefined && categories.length > 0) {
+
+            //Iterate categories object
+            categories?.find((category: any) => {
+                //Check ItemName is not Update
+                if (category?.hasOwnProperty("ItemName") && category["ItemName"] === "Update") {
+                    //Bind Attributes
+                    if (category.hasOwnProperty("ListofAttributes")) {
+                        let attr = category.ListofAttributes.Attribute;
+                        jsonCreation += `{`;
+                        for (let i = 0; i < attr.length; i++) {
+                            if (attr[i].hasOwnProperty("Name"))
+                                jsonCreation += `"${attr[i]["Name"]}":"${attr[i].hasOwnProperty("Value") ? attr[i]["Value"] : ''}",`;
+                        }
+                        jsonCreation = jsonCreation.slice(0, jsonCreation.length - 1);
+                        jsonCreation += `}`;
+                    }
+                }
+            });
+        }
+        jsonCreation += `]`;
+        console.log("StatusResponse :" + jsonCreation);
+        return jsonCreation;
+    }
+
+    private validateResponseStatus(wmResponse: any) {
+        switch (wmResponse.MessageType) {
+            case WMMessageType.Informational:
+                break;
+            case WMMessageType.Error:
+                this._route.navigate(['/errors', { outlets: { errorPage: 'error' } }], { state: { errData1: wmResponse.StatusCode, errData2: wmResponse.StatusMessage } });
+                break;
+        }
+
     }
 }
