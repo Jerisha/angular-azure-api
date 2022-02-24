9b0d2ffbff7c9eb86788e31b5d2cb98b26fa0c1e
Ashok Kumar R <Ashok.Kumarr@vodafone.com>
2022-02-24 14:33:21 +0000

    fixed spinner issue in tab-sel

--- src/app/uicomponents/table-selection/table-selection.component.ts
+++ src/app/uicomponents/table-selection/table-selection.component.ts
@@ -66,18 +66,17 @@ export class TableSelectionComponent {
 
   ngOnInit() {
     
-    //this.spinner.show();
+    this.spinner.show();
     this.dataObs$ = this.tableitem?.data;
     //Subscribing passed data from parent
     this.dataObs$.subscribe(
-      (res: any) => {
+      (res: any) => {        
         this.dataSource = new MatTableDataSource<any>(res);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-        //this.spinner.hide()
+        this.spinner.hide()
       },
-      error => { //this.spinner.hide(); 
-        console.log(error) },
+      error => { this.spinner.hide();          },
      // () => { this.spinner.hide() }
     );
    
