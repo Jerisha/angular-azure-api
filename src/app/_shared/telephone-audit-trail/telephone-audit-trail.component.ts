e10897023cc6aa6b6690953400303b3590840343
Akshay Toppo <akshay.toppo@vodafone.com>
2022-03-03 09:17:30 +0000

    Telephone audit trail api integration

--- src/app/_shared/telephone-audit-trail/telephone-audit-trail.component.ts
+++ src/app/_shared/telephone-audit-trail/telephone-audit-trail.component.ts
@@ -9,195 +9,6 @@ import { Utils } from 'src/app/_http/common/utils';
 import { Observable } from 'rxjs';
 import { map, subscribeOn } from 'rxjs/operators';
 
-const ele: TelephoneAuditTrail =
-{
-  liveRecord : undefined,
-  // liveRecord: 
-  //     {
-  //       TransactionId: '1014284011',
-  //       ParentCupid: '13',
-  //       ChildCupid: '13',
-  //       CustomerTitle: '',
-  //       CustomerForename: '',
-  //       // custName: 'SOUTH BIRMINGHAM HEALTH AUTH',
-  //       CustomerName: 'SOUTH BIRMINGHAM',
-  //       BusinessSuffix: '',
-  //       Premises: 'Edgbaston',
-  //       Thoroughfare: 'BIRMINGHAM',
-  //       Locality: '',
-  //       Postcode: 'B15 2TH',
-  //       RetailerId: '', 
-  //       AddressId: '',
-  //       AddressIdSource: '',
-  //       TransactionReference: '013/013/001014284011',
-  //       Created: 'THU 14 MAR 2019 15:26:59',
-  //       CreatedBy: 'BATCH',
-  //       Source: 'C - SAS/COMS',
-  //       Franchise: 'MCL',
-  //       SourceType: 'BATCH',
-  //       InternalAddress1: 'WOLFSON BUILDING',
-  //       InternalAddress2: 'QUEEN ELIZABETH HOSP',
-  //       InternalAddress3:	'BIRMINGHAM',
-  //       InternalAddress4: 'WEST MIDLANDS',
-  //       NewTelephoneNumber: '',
-  //       XReference: '',
-  //       LineType: 'D - DDI',
-  //     }
-  //   , 
-  unsolicitedDetails: [
-        { Code: '1046',	ErrorMessage: 'Import is 10 days overdue.',	Date: 'THU 28 JUL 2016 08:25:23',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101328071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'WED 27 JUL 2016 08:26:46',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101327071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'TUE 26 JUL 2016 08:25:20',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101326071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'MON 25 JUL 2016 08:25:19',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101325071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'SUN 24 JUL 2016 08:25:23',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101324071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'SAT 23 JUL 2016 08:25:18',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101323071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'FRI 22 JUL 2016 08:25:34',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101322071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'THU 21 JUL 2016 08:25:30',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101321071601.CAR' },
-        { Code: '1045',	ErrorMessage: 'Import Record is Missing.',	Date: 'WED 20 JUL 2016 17:55:06',	Franchise: 'EDB',	Postcode: 'SG1 1AG',	FileName: 'BT101320071606.CAR' },
-      ], 
-  transactionDetails:
-    [
-      {
-        Action: '',
-        CountTransaction: '1 A - Activate Customer',
-        Status: '110 - ERROR FINAL',
-        CreatedOn: '08-JAN-14',
-        Source: 'R - Clarify',
-        CustomerName: 'ERNST & YOUNG',
-        // email --RODDA.MANIRATHNAM@VODAFONE.COM  comment
-        details: { TransactionId: '1015002930', tranCmd: 'A - Activate Customer', btCmd: 'A - Activate Customer', parentCupid: '13', childCupid: '13', changeCupid: '13', customerTitle: ' ', customerForename: ' ', customerName: 'J2 GLOBAL UK LTD', businessSuffix: ' ', premises: 'Telehouse', thoroughfare: 'Coriander Avenue', locality: 'LONDON', postcode: 'E14 2AA', retailerId: ' ', AddressId: '', AddressIdSource: ' ', sarRefNum: ' ', sarTrnNum: ' ', reference: ' ', connType: ' ', accessMethod: ' ', prevTranId: '1014986699', transactionReference: '013/013/001015002930', status: '101-DO SEND', btSource: 'Edge', source: 'E - VA/WAD', franchise: 'VFC', orderRef: 'EDGE', CreatedOn: 'THU 08 JUL 2021 10:20:02', createdBy: ' ', sourceType: 'GUI', internalAddress1: 'TELEHOUSE EAST', internalAddress2: 'CORIANDER AVENUE', internalAddress3: 'LONDON', internalAddress4: ' ', forceValidate: 'N', NewTelephoneNumber: ' ', XReference: ' ', lineType: 'D - DDI', provide: 'THU 08 JUL 2021 10:16:00', effective: 'THU 08 JUL 2021 10:20:02', endStatus: 'THU 08 JUL 2021 10:35:01', callback: ' ', typeOfLine: 'BW - Bothway', nextTranId: '0' , comment: ' DDI RANGE- 01619526000'},
-        notificationData:
-        {
-          startTelNo: '0123467890',
-          source: 'AUDIT',
-          notificationStatus: 'Active',
-          notificationError: 'error',
-          extractedDate: 'err002',
-          updatedDate: '28/12/1992',
-          endTelNo: '0123467890',
-          orderRef: 'gahh16227',
-          CreatedOn: '20/12/1992',
-          btStatus: 'Activate',
-          btErrorCode: 'BT3456',
-          btErrorMessage: 'errorcode'
-        },
-        errorDescription: [{ ResponseCode: '2100', ResponseMessage: 'Interim transaction not sent' }],
-        commentResponse: [{ResponseCode: '1057', ResponseMessage: 'Data/Record is Invalid.', Date:'FRI 25 OCT 2013 17:26:55', IsFinal: 'Y', FileName: 'BT101303101301.CAR'}, 
-        {ResponseCode: '1018', ResponseMessage: 'OLO does not own Entry.', Date:'FRI 25 OCT 2013 17:26:55', IsFinal: 'N', FileName: 'BT101303101302.CAR'}],
-        resolution: [{Resolution: 'Superseded', CreatedBy: 'SYSTEM', CreatedOn: '08 Jul 2021', Duration: ' ', Remarks: 'Superseded by a later transaction' }]
-      },
-      {
-        Action: '',
-        CountTransaction: '2 A - Activate Customer',
-        Status: '110 - ERROR FINAL',
-        CreatedOn: '08-JAN-14',
-        Source: 'R - Clarify',
-        CustomerName: 'ERNST & YOUNG',
-        details: { TransactionId: '1015002930', tranCmd: 'A - Activate Customer', btCmd: 'A - Activate Customer', parentCupid: '13', childCupid: '13', changeCupid: '13', customerTitle: ' ', customerForename: ' ', customerName: 'J2 GLOBAL UK LTD', businessSuffix: ' ', premises: 'Telehouse', thoroughfare: 'Coriander Avenue', locality: 'LONDON', postcode: 'E14 2AB', retailerId: ' ', AddressId: '', AddressIdSource: ' ', sarRefNum: ' ', sarTrnNum: ' ', reference: ' ', connType: ' ', accessMethod: ' ', prevTranId: '1014986699', transactionReference: '013/013/001015002930', status: '101-DO SEND', btSource: 'Edge', source: 'E - VA/WAD', franchise: 'VFC', orderRef: 'EDGE', CreatedOn: 'THU 08 JUL 2021 10:20:02', createdBy: ' ', sourceType: 'GUI', internalAddress1: 'TELEHOUSE EAST', internalAddress2: 'CORIANDER AVENUE', internalAddress3: 'LONDON', internalAddress4: ' ', forceValidate: 'N', NewTelephoneNumber: ' ', XReference: ' ', lineType: 'D - DDI', provide: 'THU 08 JUL 2021 10:16:00', effective: 'THU 08 JUL 2021 10:20:02', endStatus: 'THU 08 JUL 2021 10:35:01', callback: ' ', typeOfLine: 'BW - Bothway', nextTranId: '0' , comment: ' DDI RANGE- 01619526000'},
-        notificationData:
-        {
-          startTelNo: '0123467890',
-          source: 'AUDIT',
-          notificationStatus: 'Active',
-          notificationError: 'error',
-          extractedDate: 'err002',
-          updatedDate: '28/12/1992',
-          endTelNo: '0123467890',
-          orderRef: 'gahh16227',
-          CreatedOn: '20/12/1992',
-          btStatus: 'Activate',
-          btErrorCode: 'BT3456',
-          btErrorMessage: 'errorcode'
-        },
-        commentResponse: undefined,
-        errorDescription: undefined,
-        resolution: undefined
-      }
-    ], 
-  auditDetails:
-  {
-      internalAudit: [
-      {
-        AuditActId: '0099876543',
-        TelephoneNo: '01234567890',
-        ResolutionType: 'New',
-        CliStatus: 'Active',
-        Comments: 'clarify',
-         UserComments: [
-          {
-            AuditActId: '0099876543',
-            TelephoneNo: '01234567890',
-            CreatedOn: '25/12/1992',
-            CreatedBy: 'xyz',
-            ResolutionType: 'New',
-            Comments: 'The testing is in process'
-          }
-        ] 
-      }, 
-      {
-        AuditActId: '0099876543',
-        TelephoneNo: '01234567890',
-        ResolutionType: 'New',
-        CliStatus: 'Active',
-        Comments: 'clarify',
-        UserComments: null,
-        /*
-        UserComments: [
-          {
-            AuditActId: '0099876543',
-            TelephoneNo: '01234567890',
-            CreatedOn: '25/12/1992',
-            CreatedBy: 'xyz',
-            ResolutionType: 'New',
-            Comments: 'The testing is in process'
-          }
-        ] */
-      }
-    ],
-    externalAudit: [
-      {
-        AuditActId: '21 - 03 APR 2019',
-        TelephoneNo: '01619526181',
-        ResolutionType: 'Auto Resolved',
-        CliStatus: 'Matched',
-        Comments: 'show',
-        UserComments: null,
-    },
-    {
-        AuditActId: '21 - 03 APR 2019',
-        TelephoneNo: '01619526181',
-        ResolutionType: 'Auto Resolved',
-        CliStatus: 'Matched',
-        Comments: 'show',
-        UserComments: [{
-          AuditActId: '28',
-          TelephoneNo: '01412702810',
-          CreatedOn: '21-NOV-20 02.32.17.040358 PM',
-          CreatedBy: 'SYSTEM',
-          ResolutionType: 'Auto Closed',
-          Comments: 'Auto closed occurs when a new Audit run is generated.'
-        }],
-    }
-    ],
-    fullAudit: [
-      {
-        AuditActId: '28 - 29 AUG 2020',
-      TelephoneNo: '01619526181',
-      ResolutionType: 'Auto Closed',
-      ExternalCliStatus: 'S-Matched',
-      FullAuditCliStatus: 'SAD-Matched - Source Active MisMatched',
-      UserComments: [{ AuditActId: '28',
-      TelephoneNo: '01619526181',
-      CreatedOn: '21-NOV-20 02.33.46.878459 PM',
-      CreatedBy: 'SYSTEM',
-      ResolutionType: 'Auto Closed',
-      Comments: 'Auto closed occurs when a new audit run is generated.', }], 
-      }
-    ]  
-  } 
-} 
-
-
 
 @Component({
   selector: 'app-telephone-audit-trail',
@@ -221,19 +32,9 @@ export class TelephoneAuditTrailComponent implements OnInit {
   
   step: number = 2;
 
-  Telephone?: TelephoneAuditTrail[];
   addressDetails = new AddressDetails();
-  
-  liverecord?: LiveRecord;
 
   auditTrailReport$!: Observable<any>;
-  liverecord$!: Observable<any>;
-  detailedReport$!: Observable<any>;
-  detailedReport: any;
-  unsolicitedDetails?: UnsolicitedDetails[];
-  transactionDetails?: TransactionDetails[];
-  auditDetails?: AuditDetails;
-  GetResult$!: Observable<any>;
 
   @Input() isUnsol: boolean = false;
   @Input() telNum!: number ;
@@ -249,27 +50,26 @@ export class TelephoneAuditTrailComponent implements OnInit {
     // }
   }
 
-  columnsToDisplay = ["Action", "CountTransaction", "Status", "CreatedOn", "Source", "CustomerName"];
-  auditTrailInternalDisplay:string[]=['AuditActId','TelephoneNo','ResolutionType','CliStatus','UserComments'];
-  fullAuditTrailDisplay: string[]=['AuditActId','TelephoneNo','ResolutionType','ExternalCliStatus', 'FullAuditCliStatus', 'UserComments'];
+  dataColumns = ["Action", "CntTransaction", "Status", "Created", "Source", "CustomerName"];
+  // columnsToDisplay = ["Action", "Count Transaction", "Status", "Created On", "Source", "Customer Name"];
+ 
+  columnsToDisplay = [ {header:'Action', headerValue: 'Action'},
+  {header:'Count Transaction', headerValue: 'CntTransaction'},
+  {header:'Status', headerValue: 'Status'},
+  {header:'Created On', headerValue: 'Created'},
+  {header:'Source', headerValue: 'Source'},
+  {header:'Customer Name', headerValue: 'CustomerName'}];
+ 
+  auditTrailInternalDisplay:string[]=['AuditActId','TelephoneNumber','ResolutionType','CliStatus','UserComment'];
+  fullAuditTrailDisplay: string[]=['AuditActId','TelephoneNumber','ResolutionType','ExternalCliStatus', 'FullAuditCliStatus', 'UserComment'];
 
   ngOnInit(): void {
-    // console.log("telephone No " + this.telephoneNumber);
-    // this.liverecord =  ele.liveRecord;
-    this.transactionDetails = ele?.transactionDetails;
-    this.auditDetails = ele.auditDetails;
-    this.unsolicitedDetails = ele?.unsolicitedDetails;
+
+
     // let request = Utils.prepareGetRequest("TelephoneNumberAuditTrail", "SolicitedErrors", [{  Name : "TelephoneNumber",
     // Value : [ "02071117402" ] }]);
     // let request = Utils.prepareGetRequest("TelephoneNumberAuditTrail", "SolicitedErrors", [{  Name : "TelephoneNumber",
     // Value : [ this.telephoneNumber ] }]);
-
-   // this.GetResult$ = this.service.getDetails(request);
-
-    // debugger;
-    //  this.service.getDetails(request).subscribe((res: any) => 
-    //  console.log(res)
-    //  );
 
   }
 
@@ -278,45 +78,12 @@ export class TelephoneAuditTrailComponent implements OnInit {
     // console.log(changes);
     if(changes.telNum.currentValue != changes.telNum.previousValue)
     {
+      this.setStep(2);
       let request = Utils.prepareGetRequest("TelephoneNumberAuditTrail", "SolicitedErrors", [{  Name : "TelephoneNumber",
-    // Value : [ this.telNum ] }]);
-    Value : [ "02075957399" ] }]);
+    Value : [ this.telNum ] }]);
+    // Value : [ "02075957399" ] }]);
 
-    // this.auditTrailReport$ = this.service.getDetails(request).pipe(map((res: any) => res[0]));
-      // this.liverecord = JSON.parse(JSON.stringify(this.auditTrailReport$));
-      // console.log("New live data " + JSON.stringify(this.auditTrailReport$));
-
-      this.service.getDetails(request).subscribe(
-         (res:any) => {
-        this.detailedReport = res[0].LiveDetails[0];
-      },
-      err => { console.error(err);
-      },
-       () => {
-         console.log("hello");
-        }
-          );
-
-      console.log("detailed data" + this.detailedReport);
-    //  this.service.getDetails(request).pipe(map((res: any) =>{
-    //   this.liverecord$ =  res[0].LiveDetails[0];
-    //   console.log("Live details " + JSON.stringify(res[0].LiveDetails[0]));
-      // this.detailedReport$ = res[0].AuditDetailedReport[0].DetailedReport[0];
-      // console.log("Detailed Report " + res[0].AuditDetailedReport[0].DetailedReport[0]);
-      // return res[0];
-    // }));
-    // this.liverecord$ = this.auditTrailReport$.pipe(map((res: any) => res.LiveDetails[0]));
-    // this.detailedReport$ = this.auditTrailReport$.pipe(map((res: any) => res.AuditDetailedReport[0].DetailedReport[0]));
-    // this.detailedReport$ = this.service.getDetails(request).pipe(map((res: any) =>{
-    //   return res[0].AuditDetailedReport[0].DetailedReport[0];
-    // }));
-
-
-
-    //   this.liverecord$ = this.auditTrailReport$.pipe(map((res: any) =>{
-    //      console.log(res.LiveDetails[0]);
-    // //  console.log(res[0].LiveDetails[0]);
-    // }));
+    this.auditTrailReport$ = this.service.getDetails(request).pipe(map((res: any) => res[0]));
 
     }
   }
@@ -338,15 +105,15 @@ export class TelephoneAuditTrailComponent implements OnInit {
   
   setAddressDetails(section: string, element?: any) {
     // console.log(element.details.postcode);
-    if(section == 'transactionDetails') {
+    if(section === 'TransactionDetails') {
       this.addressDetails.isData = true;
-    this.addressDetails.postcode = element.details.postcode;
-    this.addressDetails.CustomerName = element.details.CustomerName;
-    this.addressDetails.internalAddr1 = element.details.internalAddr1;
-    this.addressDetails.internalAddr2 = element.details.internalAddr2;
-    this.addressDetails.internalAddr3 = element.details.internalAddr3;
-    this.addressDetails.internalAddr4 = element.details.internalAddr4;
-    } else if( section == 'removeAddress') {
+    this.addressDetails.postcode = element.Postcode;
+    this.addressDetails.CustomerName = element.CustomerName;
+    this.addressDetails.internalAddr1 = element.InternalAddress1;
+    this.addressDetails.internalAddr2 = element.InternalAddress2;
+    this.addressDetails.internalAddr3 = element.InternalAddress3;
+    this.addressDetails.internalAddr4 = element.InternalAddress4;
+    } else if( section === 'RemoveAddress') {
       this.addressDetails = new AddressDetails();
     }
     // console.log(this.addressDetails);
@@ -355,20 +122,6 @@ export class TelephoneAuditTrailComponent implements OnInit {
   expandDisplayedColumns = ["optionName", "optionDescription"];
   expandedElement: PeriodicElement | null | undefined;
 
-  // toTableItem(item: any): TableItem {
-    
-
-  //   let array = [];
-  //   let thisItem = item[0];
-  //   for (let key in thisItem) {
-  //     if (thisItem.hasOwnProperty(key)) {
-  //       array.push(key);
-  //     }
-  //   }
-  //   return  { data: item, dataColumns: array };
-  //   //console.log(table);
-     
-  // }
 
 }
 
