c5030f96d6060cbf547de0dedf5d1caa5193defc
Akshay Toppo <akshay.toppo@vodafone.com>
2022-03-17 06:45:05 +0000

    UI comments for audit trail

--- src/app/auditreports/auditreports-routing.module.ts
+++ src/app/auditreports/auditreports-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuditdiscrepancyreportComponent, AuditexcelreportsComponent, FullauditdetailsComponent } from './index';
+import { AuditdiscrepancyreportComponent, AuditexcelreportsComponent, FullauditdetailsComponent, FullAuditHistoryComponent } from './index';
 const routes: Routes = [
   {
     path: 'auditdiscrepancyreport',
@@ -8,6 +8,7 @@ const routes: Routes = [
   },
   { path: 'auditexcelreport', component: AuditexcelreportsComponent },
   { path: 'fullauditdetails', component: FullauditdetailsComponent },
+  { path: 'fullaudithistory', component: FullAuditHistoryComponent },
 ];
 
 @NgModule({
