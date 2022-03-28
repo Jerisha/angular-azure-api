67b073e518a4dcc234b4c97d8287ccd13182b1f1
y852133 <deepalakshmi.v@vodafone.com>
2022-03-28 15:41:03 +0100

    tabs added

--- src/app/report-references/report-reference-main/report-reference-main.component.ts
+++ src/app/report-references/report-reference-main/report-reference-main.component.ts
@@ -1,9 +1,10 @@
 
-import { Component,  OnInit, AfterViewInit, ChangeDetectorRef, SimpleChanges, Input } from '@angular/core';
+import { Component,  OnInit, AfterViewInit, ChangeDetectorRef, SimpleChanges, Input, Attribute } from '@angular/core';
 import { ReportReferenceService } from '../report-reference.service';
 import { FormBuilder} from '@angular/forms';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { IColoumnDef } from '../IControls';
+import { Tab } from 'src/app/uicomponents/models/tab';
 
 @Component({
   selector: 'app-report-reference-main',
@@ -27,7 +28,9 @@ export class ReportReferenceMainComponent implements OnInit, AfterViewInit{
   showDataForm:boolean =false;
   showDetailsForm:boolean =false;
   data:any =[];
+  data1:any =[];
   displayedColumns:any =[];
+  displayedColumns1:any =[];
   lstFields: IColoumnDef[] = [];
 
   isShow:boolean =false;
@@ -35,6 +38,9 @@ export class ReportReferenceMainComponent implements OnInit, AfterViewInit{
   record!: null;
   eventName!: string;  
   reportIndex!: number;
+  showDetails: boolean = false;
+  selectedTab: number = 0;
+  tabs: Tab[] = [];
 
   onMenuClicked(){
     this.showMenu = this.showMenu == 'expanded' ? 'collapsed' : 'expanded';    
@@ -52,16 +58,61 @@ export class ReportReferenceMainComponent implements OnInit, AfterViewInit{
     this.showDetailsForm =true;
     this.isShow =true;
     this.displayedColumns=[];
+    this.displayedColumns1=[];
     this.data =[];
+    this.data1=[];
     let dispVal = this.reportReferenceService.displayedColumns[this.reportIndex][this.reportName]; 
     this.displayedColumns=  dispVal ==undefined?[]:dispVal; 
     let dat =this.reportReferenceService.data[this.reportIndex][this.reportName];
     this.data =dat ==undefined?[]:dat;
+    // }
     // this.lstFields =[];
    // this.lstFields =this.reportReferenceService.setForm(this.reportName);
+    this.newTab();
     
+
   }
 
+  Onselecttabchange($event: any){
+
+    let reportName  = this.tabs.find(x => x.tabType == $event.index)?.name;
+    reportName =reportName == undefined? '':reportName
+    let reportIndex  = this.reportNames.findIndex(x => x == reportName);
+    let dispVal = this.reportReferenceService.displayedColumns[reportIndex][reportName]; 
+    this.displayedColumns=  dispVal ==undefined?[]:dispVal; 
+    let dat =this.reportReferenceService.data[reportIndex][reportName];
+    this.data =dat ==undefined?[]:dat;
+    console.log('tes2',$event.index);
+  }
+  newTab() {
+    // if (this.tabs === []) return;
+  
+        if (!this.tabs?.find(x => x.name == this.reportName)) {
+          this.tabs.push({
+            // tabType: this.reportIndex,
+            tabType: this.tabs.length,
+            name: this.reportName,
+          });
+          console.log('test',this.tabs,this.selectedTab);
+          this.selectedTab = this.tabs.findIndex(x => x.name == this.reportName) + 1;
+          console.log('test',this.tabs,this.selectedTab);
+        }
+         else{
+
+          this.selectedTab = this.tabs.findIndex(x => x.name == this.reportName) + 1;
+          console.log('test',this.tabs,this.selectedTab);
+         
+        }
+        console.log('test',this.tabs,this.selectedTab);
+    }
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+    this.showDetails = this.tabs.length > 0 ? true : false;
+    if(this.tabs.length == 0) {
+    this.isShow = false;
+    this.showMenu = 'expanded';
+    }
+  }
 onCreateRecord()
   {
     this.record =null;
