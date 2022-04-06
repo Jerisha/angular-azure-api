65e09d7d47e0595518b4564057253e3e9594ac4d
Ashok Kumar R <Ashok.Kumarr@vodafone.com>
2022-04-06 05:06:36 +0100

    edited

--- src/app/_helper/pipe/drop-down-list-filter.pipe.ts
+++ src/app/_helper/pipe/drop-down-list-filter.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DropDownListFilterPipe implements PipeTransform {
     transform(items: any, strSearch: string): any {
         if (items !== null && (strSearch !== '' || strSearch != undefined)) {            
-            var filterValue = strSearch;
+            var filterValue = strSearch.toLowerCase();
             let filteredList = items.filter((x: any) => x.toLowerCase().indexOf(filterValue) === 0)
             return filteredList;
         }
