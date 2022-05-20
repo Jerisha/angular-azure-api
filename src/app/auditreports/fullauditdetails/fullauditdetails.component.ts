9b7535025cbad8e2acece10d11f2d7ac427528d1
Prasanth Kumar <Prasanth.Kumar@vodafone.com>
2022-05-20 09:24:02 +0100

    Cosmetic changes

--- src/app/auditreports/fullauditdetails/fullauditdetails.component.ts
+++ src/app/auditreports/fullauditdetails/fullauditdetails.component.ts
@@ -191,7 +191,7 @@ export class FullauditdetailsComponent implements OnInit, AfterViewInit {
     { headerValue: 'CDMS/NMSRAreacall', header: 'CDMS/NMSR Areacall', showDefault: true, isImage: false },
     { headerValue: 'IsVodafoneRangeHolder', header: 'Is VodafoneRange Holder', showDefault: true, isImage: false },
     { headerValue: 'BTCustomer', header: 'BT Customer', showDefault: true, isImage: false },
-    { headerValue: 'BTPostcode', header: 'BTP ostcode', showDefault: true, isImage: false },
+    { headerValue: 'BTPostcode', header: 'BT Postcode', showDefault: true, isImage: false },
     { headerValue: 'BTLocality', header: 'BT Locality', showDefault: true, isImage: false },
     { headerValue: 'BTPremise', header: 'BT Premise', showDefault: true, isImage: false },
     { headerValue: 'BTThouroughfare', header: 'BT Thourough fare', showDefault: true, isImage: false },
