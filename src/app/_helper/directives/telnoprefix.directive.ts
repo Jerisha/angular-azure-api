01d362c6c90d90286541c3061f0039e3a594a6e4
Prasanth Kumar <Prasanth.Kumar@vodafone.com>
2022-05-23 11:31:42 +0100

    Manual corrections validation implemented

--- src/app/_helper/directives/telnoprefix.directive.ts
+++ src/app/_helper/directives/telnoprefix.directive.ts
@@ -1,26 +1,30 @@
 import { Directive, Input } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn } from '@angular/forms';
+import { isNumeric } from 'rxjs/internal-compatibility';
 
 @Directive({
     selector: '[telNoPrefixValidator]',
     providers: [{ provide: NG_VALIDATORS, useExisting: TelphoneNoValidatorDirective, multi: true }]
 })
 export class TelphoneNoValidatorDirective implements Validator {
-
     @Input() prefix?: string[] = ['01', '02', '03', '08'];
     validate(control: AbstractControl): { [key: string]: any } | null {
 
-
         const selection: any = control.value ? control.value : null;
         if (selection != null) {
-            if (selection?.charAt(0) != 0)
-                control.setValue(selection?.length <= 10 ? '0' + selection : selection);
+            if (isNumeric(parseInt(selection))) {
+                if (selection?.charAt(0) != 0)
+                    control.setValue(selection?.length <= 10 ? '0' + selection : selection);
 
-            if (selection && (this.prefix?.indexOf(selection.substring(0, 2)) === -1) && selection.length >= 2) {
+                if (selection && (this.prefix?.indexOf(selection.substring(0, 2)) === -1) && selection.length >= 2) {
+                    return { invalidPrefix: true };
+                }
+            }
+            else {
+                control.setValue('0');
                 return { invalidPrefix: true };
             }
         }
         return null;
-
     }
 }
