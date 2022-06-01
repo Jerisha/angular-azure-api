c77ad9d142a7bfc9d91d2076cce38d29474ed45b
Dev Joshi <dev.joshi@vodafone.com>
2022-06-01 09:16:53 +0100

    telrngrep ui chng

--- src/app/reports/telephone-range-report/telephone-range-report.component.ts
+++ src/app/reports/telephone-range-report/telephone-range-report.component.ts
@@ -67,7 +67,6 @@ export class TelephoneRangeReportComponent implements OnInit {
     private cdr: ChangeDetectorRef,
     private telnoPipe: TelNoPipe) {}
 
-  @ViewChild('table1') table1?:TableSelectionComponent;
   myTable!: TableItem;
   dataSaved = false;
   selectListItems: string[] = [];
