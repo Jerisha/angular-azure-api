0cced38c841adf8d14dfdd828ee67d7cc2e56b02
y852133 <deepalakshmi.v@vodafone.com>
2022-06-10 14:48:45 +0100

    Special cease transaction

--- src/app/transactions/special-cease-transaction/current-live-details.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-    selector: 'app-current-live-details',
-    templateUrl: './current-live-details.component.html',
-    styleUrls: ['./current-live-details.component.css']
-  })
-  export class CurrentLiveDetailsComponent  {
-  }
