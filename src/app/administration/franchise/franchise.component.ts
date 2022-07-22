75fe3dcb9050c40fca798b5040abe169ddb7d0ab
Ashok <ashok.yarlagadda@vodafone.com>
2022-07-22 07:34:02 +0100

    Franchise

--- src/app/administration/franchise/franchise.component.ts
+++ src/app/administration/franchise/franchise.component.ts
@@ -47,7 +47,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     position: 1,
     name: 'Hydrogen',
     weight: 1.0079,
-    symbol: 'H',
+    symbol: 'BLG BLG-CBL',
     description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
         atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
   },
@@ -55,7 +55,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     position: 2,
     name: 'Helium',
     weight: 4.0026,
-    symbol: 'He',
+    symbol: 'BLG BLG-CBL',
     description: `Helium is a chemical element with symbol He and atomic number 2. It is a
         colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
         group in the periodic table. Its boiling point is the lowest among all the elements.`,
@@ -64,7 +64,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     position: 3,
     name: 'Lithium',
     weight: 6.941,
-    symbol: 'Li',
+    symbol: 'BLG BLG-CBL',
     description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
         lightest solid element.`,
@@ -73,7 +73,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     position: 4,
     name: 'Beryllium',
     weight: 9.0122,
-    symbol: 'Be',
+    symbol: 'BBLG BLG-CBL',
     description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
         relatively rare element in the universe, usually occurring as a product of the spallation of
         larger atomic nuclei that have collided with cosmic rays.`,
