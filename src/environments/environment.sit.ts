800d8f05f8b712c257f72630f0a04160f64a1eff
Ashok Kumar R <Ashok.Kumarr@vodafone.com>
2022-09-06 07:56:08 +0100

    env config

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  UnsolTransactionDays:'62',
+  UnsolTransactionDays: '62',
   production: false,
-  api_auth:'https://ukont1vr.ad.plc.cwintra.com:6054/userProfiles/',
- 
-  api_al :'https://ukont1vr.ad.plc.cwintra.com:6054/pythonAPI/',
-
+  api_auth: 'https://ukont1vr.ad.plc.cwintra.com:6054/userProfiles/',
+  // api_al :'https://ukont1vr.ad.plc.cwintra.com:6054/pythonAPI/'
+  api_al: 'https://ukrpirvr.ad.plc.cwintra.com:8044/pythonAPI/' // old
 };
+
+
