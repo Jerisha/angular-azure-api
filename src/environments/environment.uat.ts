be53a87fcec64aa3742baa3e5d66bae43b62a9e3
y852133 <deepalakshmi.v@vodafone.com>
2022-09-23 07:53:07 +0100

    change in the python url

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -2,7 +2,8 @@ export const environment = {
   UnsolTransactionDays: '62',
   production: false,
   api_auth: 'https://ukont2vr.ad.plc.cwintra.com:6054/userProfiles/',
-  // api_al :'https://ukont2vr.ad.plc.cwintra.com:6054/pythonAPI/',
-  api_al: 'https://ukrpirvr.ad.plc.cwintra.com:8044/pythonUATAPI/' //old
+  api_al :'https://ukont2vr.ad.plc.cwintra.com:6054/pythonAPI/',
+  
+ // api_al: 'https://ukrpirvr.ad.plc.cwintra.com:8044/pythonUATAPI/' //old
 
 };
