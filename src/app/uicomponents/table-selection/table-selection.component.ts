e4b1b77042ef00bc7bcb2388169c3ca26cb664d9
Akshay Toppo <akshay.toppo@vodafone.com>
2022-12-02 13:48:42 +0000

    auditUseraction, AuditStatusTracker column header

--- src/app/uicomponents/table-selection/table-selection.component.ts
+++ src/app/uicomponents/table-selection/table-selection.component.ts
@@ -407,7 +407,7 @@ export class TableSelectionComponent extends UserProfile implements OnDestroy, A
         this.select.options.forEach((item: MatOption) => item.select());
       } else {
         this.select.options.forEach((item: MatOption, index) => {
-          if (item.value === 'TelephoneNumber' || item.value === 'View' || item.value === 'Link' || item.value === 'Links' || item.value === 'StartTelephoneNumber') {
+          if (item.value === 'TelephoneNumber' || item.value === 'View' || item.value === 'Link' || item.value === 'Links' || item.value === 'StartTelephoneNumber' || (this.reportIdentifier === 'AuditStatusTracker' && item.value ==='ActId') || (this.reportIdentifier === 'AuditUserActionSummary' && item.value ==='AuditACTID' )) {
             //  item.deselect();
             ;
           } else {
